c466e843857d38704a9bbe4f835f3dab6422ce00
Eman Radwan <emanalaa11499@gmail.com>
2025-11-26 23:06:01 +0200

    edit style

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFound: React.FC = () => {
   return (
-    <p className="text-center font-medium text-pink-900 text-2xl">
+    <p className="text-center font-medium text-pink-900 text-xl md:text-2xl">
       No appointments found for today! Start adding a new one.
     </p>
   );
